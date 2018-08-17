dce7a68585b63db4027c89f40dbbc1c109086f45
常露阳 <changluyang@hualala.com>
2018-08-17 09:41:14 +0800

    no message

---
+++ DotStyleTextField/Code/DotStyleTextField/DotStyleTextField.h
@@ -0,0 +1,18 @@
+//
+//  DotStyleTextField.h
+//  DotStyleTextField
+//
+//  Created by ChangLuyang on 2018/5/2.
+//  Copyright © 2018年 ChangLuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DotStyleTextField;
+typedef void(^InputFinishBlock)(DotStyleTextField *textField, NSString *inputText);
+
+IB_DESIGNABLE
+@interface DotStyleTextField : UITextField
+@property (nonatomic, copy) InputFinishBlock inputFinishHandler;
+- (instancetype)initWithFrame:(CGRect)frame;
+@end
